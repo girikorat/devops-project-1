447e54971ec59f8a59d1f43f3f5259da329a0cc3
girikorat <162902442+girikorat@users.noreply.github.com>
2024-03-10 23:31:15 +0530

    Add files via upload

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
+    bucket = "dev-proj-1-remote-state-bucket1"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-west-1"
   }
